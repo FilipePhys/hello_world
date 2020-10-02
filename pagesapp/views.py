62e3d487bc52a04cbc68a96a08a56db9f922ce2e
FilipePhys <Filipebfisica@gmail.com>
2020-10-02 18:56:59 -0300

    Included home, about and test

--- pagesapp/views.py
+++ pagesapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse#me
+from django.views.generic import TemplateView#me
+
 
 # Create your views here.
-def homePageView(request):
-    return HttpResponse('Hello, World! É com vc mesmo Pableira!!!')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
